9fa00aa5310fed507505eb064274bfe20a2e3a7e
Sherwin727 <corralessherwin@gmail.com>
2024-10-12 00:03:14 +0800

    Adding the Profile Features
    Test

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Home = () => {
+return (
+<div>
+<h2>Home</h2>
+<p>Welcome to the Home page</p>
+<p>Test WHAT DOES THIS SAY MOTHERFUCKER?</p>
+</div>
+);
+}
+export default Home;
